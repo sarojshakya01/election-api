e1a744a2c28b93cbffaec11dc1cc094389f57346
Saroj Shakya <saroj@Sarojs-MacBook-Pro.local>
2022-10-31 09:05:13 +0545

    schema changed

--- data/db/schemas.sql
+++ data/db/schemas.sql
@@ -36,6 +36,10 @@ CREATE TABLE IF NOT EXISTS ds_election_regions (
     rtype ENUM ('federal','provincial') NOT NULL default "federal",
     name_np varchar(100) NOT NULL,
     name_en varchar(100) NOT NULL,
+    total_voter int DEFAULT 0,
+    area_sq_km float DEFAULT 0,
+    voting_booth int DEFAULT 0,
+    election_expense_limit float DEFAULT 0,
     created_at timestamp default now(), 
     updated_at timestamp default now() on update now(),
     PRIMARY KEY (id),
@@ -151,5 +155,5 @@ CREATE VIEW ds_v_provincial_results AS SELECT JSON_ARRAYAGG(districts) AS provin
 DROP VIEW IF EXISTS ds_v_district_regions;
 DROP VIEW IF EXISTS ds_v_province_district_regions;
 
-CREATE view ds_v_district_regions AS SELECT d.province_id, d.district_id, d.name_np, d.name_en, d.total_fregions, d.total_pregions, JSON_ARRAYAGG(JSON_OBJECT("id", ROUND(r.region_id, 1), "rtype", r.rtype, "name_np", r.name_np, "name_en", r.name_en)) AS "regions" FROM ds_election_districts d, ds_election_regions r WHERE r.district_id = d.district_id GROUP BY d.district_id ORDER by d.id;
+CREATE view ds_v_district_regions AS SELECT d.province_id, d.district_id, d.name_np, d.name_en, d.total_fregions, d.total_pregions, JSON_ARRAYAGG(JSON_OBJECT("id", ROUND(r.region_id, 1), "rtype", r.rtype, "name_np", r.name_np, "name_en", r.name_en, "total_voter", r.total_voter, "area_sq_km", r.area_sq_km, "voting_booth", r.voting_booth, "election_expense_limit", r.election_expense_limit)) AS "regions" FROM ds_election_districts d, ds_election_regions r WHERE r.district_id = d.district_id GROUP BY d.district_id ORDER by d.id;
 CREATE VIEW ds_v_province_district_regions AS SELECT p.province_id, p.name_np, p.name_en, p.color, JSON_ARRAYAGG(JSON_OBJECT("id", dr.district_id, "name_np", dr.name_np, "name_en", dr.name_en, "total_fregion", dr.total_fregions, "total_pregion", dr.total_pregions, "regions", dr.regions)) AS "districts" FROM ds_election_provinces p, ds_v_district_regions dr WHERE p.province_id = dr.province_id GROUP BY p.province_id;
